8f169e985c06dd3e00434221fe197eb9b7e4f6df
codehimanshu <himanshuagrawal1998@gmail.com>
2022-03-30 18:56:44 +0530

    add score calculation:

--- app/src/main/java/com/example/myapplication/ResultScreen.kt
+++ app/src/main/java/com/example/myapplication/ResultScreen.kt
@@ -10,18 +10,14 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class ResultScreen : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,6 +34,7 @@ class ResultScreen : ComponentActivity() {
                         .fillMaxSize()
                         .background(Color(0xFF353a65)),
                 ) {
+                    Text("$score")
                     Box(modifier = Modifier
                         .background(Color.White, shape = CircleShape)
                         .height(200.dp)
