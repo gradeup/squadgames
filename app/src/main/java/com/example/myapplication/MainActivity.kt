b827983310b79a553dd13dfabed9a4d689f9c6fa
Anjana Bajaj <bajajanjana@Anjanas-MacBook-Air-2.local>
2022-03-30 16:11:27 +0530

    start nd end screen added

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,25 +5,91 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-                    Welcome("Welcome")
+            val context = LocalContext.current
+            val scaffoldState= rememberScaffoldState()
+            var textFieldState by remember {
+                mutableStateOf(value="")
+            }
+            val scope= rememberCoroutineScope()
+            Snackbar {
+                Scaffold(modifier = Modifier.fillMaxSize(),
+                    scaffoldState= scaffoldState
+                ) {
+                    Column(horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center, modifier = Modifier
+                            .fillMaxSize()
+                            .background(Color(0xFF353a65)),
+                    ) {
+                        TextField(value = textFieldState ,
+                            label = {
+                                Text(text = "Enter your Name")
+                            },
+                            onValueChange = {
+                                textFieldState= it
+                            },
+                            singleLine = true,
+                            modifier = Modifier
+                                .height(50.dp)
+                                .background(Color.White,shape = RoundedCornerShape(50))
+
+                        )
+                        Spacer(modifier = Modifier.height(12.dp))
+                        Box(modifier = Modifier
+                            .background(
+                                Brush.horizontalGradient(
+                                    listOf(
+                                        Color(0xFFa54776),
+                                        Color(0xFFd45dad),
+                                        Color(0xFFc468da)
+                                    )
+                                ), shape = RoundedCornerShape(50)
+                            )
+                            //.background(Color(0xFFea4c89), shape = RoundedCornerShape(50))
+                            .height(50.dp)
+                            .width(270.dp)
+                            .clickable { context.startActivity(
+                                Intent(context, QuizActivity::class.java)
+                            ) }
+                        ){
+                            Text("Start My Quiz", Modifier
+                                .align(Alignment.Center),
+                                color = Color.White,
+                                fontSize = 20.sp,
+                            )
+                        }
+                    }
+                }
+
+            }
         }
+
     }
 }
+
 
 @Composable
 fun Welcome(name: String) {
@@ -34,10 +100,3 @@ fun Welcome(name: String) {
     })
 }
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    MyApplicationTheme {
-        Greeting("Android")
-    }
-}
--- app/src/main/java/com/example/myapplication/ResultScreen.kt
+++ app/src/main/java/com/example/myapplication/ResultScreen.kt
@@ -5,14 +5,22 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class ResultScreen : ComponentActivity() {
@@ -20,34 +28,50 @@ class ResultScreen : ComponentActivity() {
         super.onCreate(savedInstanceState)
         var score:Int;
         setContent {
-
             Column() {
                 score = intent.getIntExtra("score", 0)
-                Greeting("Wohooo, your score is $score!")
-                Reset("Retake Quiz")
+                val context = LocalContext.current
+                val activity = (LocalContext.current as? Activity)
+                Column(horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(Color(0xFF353a65)),
+                ) {
+                    Box(modifier = Modifier
+                        .background(Color.White, shape = CircleShape)
+                        .height(200.dp)
+                        .width(200.dp)
+                    )
+                    Spacer(modifier = Modifier.height(40.dp))
+                    Box(modifier = Modifier
+                        .background(
+                            Brush.horizontalGradient(
+                                listOf(
+                                    Color(0xFFa54776),
+                                    Color(0xFFd45dad),
+                                    Color(0xFFc468da)
+                                )
+                            ), shape = RoundedCornerShape(50)
+                        )
+                        //.background(Color(0xFFea4c89), shape = RoundedCornerShape(50))
+                        .height(60.dp)
+                        .width(300.dp)
+                        .clickable {
+                            context.startActivity(
+                                Intent(context, MainActivity::class.java)
+                            )
+                            activity?.finish()
+                        }
+                    ){
+                        Text("Play Again", Modifier
+                            .align(Alignment.Center),
+                            color = Color.White,
+                            fontSize = 25.sp,
+                        )
+                    }
+                }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Composable
-fun Reset(name: String) {
-//    val context = LocalContext.current
-    val activity = (LocalContext.current as? Activity)
-    Button(onClick = { activity?.finish() }) {
-        Text(text = "$name!")
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview2() {
-    MyApplicationTheme {
-        Greeting("Android")
     }
 }
